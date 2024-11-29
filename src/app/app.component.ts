6c489955a5c8d6d91c4c1277f70fb67894146f4c
banu005 <banumathey5@gmail.com>
2024-11-29 15:44:09 +0100

    chap2.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Router, RouterLink, RouterOutlet} from '@angular/router';
+import {CookieComponent} from './cookie/cookie.component';
+import {TatoueurComponent} from './components/tatoueur/tatoueur.component';
+import {MatSidenav, MatSidenavContainer, MatSidenavContent} from '@angular/material/sidenav';
+import {MatListItem, MatNavList} from '@angular/material/list';
+import {MatIcon} from '@angular/material/icon';
+import {MatToolbar} from '@angular/material/toolbar';
+import {HttpClient} from '@angular/common/http';
+import {LoginComponent} from './components/login/login.component';
+import {AuthService} from './services/auth.service';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TatoueurComponent, MatSidenavContent, MatListItem, MatIcon, MatNavList, MatSidenav, MatSidenavContainer, MatToolbar],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Chap2Angular';
+  title = 'Banu';
+  date=new Date();
+
+//Ce code appelle notre service d’authentification et récupère un booléen : si l’user
+// est connecté ou non.
+  constructor(private auth: AuthService, private http: HttpClient, private router: Router) {
+  }
+  authenticated() { return this.auth.authenticated; }
+
+  logout(){this.auth.logout();}
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import {TatoueurComponent} from './components/tatoueur/tatoueur.component';
+import {LoginComponent} from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'tatoueurs',
+    title: 'Nos tatoueurs',
+    component: TatoueurComponent,
+  },
+  {
+    path: 'login',
+    title: 'les logins',
+    component: LoginComponent,
+  }
+];
+
+
