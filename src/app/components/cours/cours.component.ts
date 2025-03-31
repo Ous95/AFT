ef9fa7fffec511377860c471dcbaf1eb40d32b40
ousman <ous95200@gmail.com>
2025-03-31 17:28:43 +0200

    sava

--- src/app/components/cours/cours.component.ts
+++ src/app/components/cours/cours.component.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
 import {MatTable} from '@angular/material/table';
 import {MatListItem} from '@angular/material/list';
+import {MatChipRow} from '@angular/material/chips';
+import {MatExpansionPanelActionRow} from '@angular/material/expansion';
+
+import {MatGridList, MatGridListModule, MatGridTile} from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-cours',
   imports: [
     RouterLink,
+    MatChipRow,
+
+    MatGridTile,
+    MatGridList,
 
 
   ],
@@ -16,3 +24,5 @@ import {MatListItem} from '@angular/material/list';
 export class CoursComponent {
 
 }
+
+
